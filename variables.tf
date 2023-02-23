6f74e8c89f6d72adea4610eccd187d2940330a8f
naveenkumar sivarajan <sivarajan.naveenkumar@mahindra.com>
2023-02-23 16:42:26 +0530

    initial commit

--- variables.tf
+++ variables.tf
@@ -1,37 +1,29 @@
-variable "group_name" {
+variable "resource_group_name" {
   type        = string
-  description = "The Name which should be used for this Resource Group. Changing this forces a new Resource Group to be created."
+  description = "name of the resource group"
 }
 
-variable "storage_account_name" {
+variable "location" {
   type        = string
-  description = "name of the storage account"
+  description = "location of the resource group"
 }
 
-variable "file_storage_account_replication_type" {
+variable "mssql_name" {
   type        = string
-  description = "replication type of file storage, LRS in uat and GRS in prod"
+  description = "name of the mssql server"
 }
 
-variable "container_set" {
-  description = "List of containers to create and their access levels."
-  type        = list(object({ name = string, access_type = string }))
+variable "database_name" {
+  type        = string
+  description = "name of the database_name "
 }
 
-variable "file_storage_list" {
-  description = "List of containers to create and their access levels."
-  type        = list(object({ name = string, quota = number }))
-  default     = []
+variable "administrator_login" {
+  type        = string
+  description = "name of the administrator_login"
 }
 
-variable "queues_set" {
-  description = "List of storages queues"
-  type        = list(object({ name = string }))
-  default     = []
-}
-
-variable "tables_set" {
-  description = "List of storage tables."
-  type        = list(object({ name = string }))
-  default     = []
+variable "administrator_login_password" {
+  type        = string
+  description = "name of the administrator_login_password"
 }

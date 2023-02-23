a3c5c6894ba0acfeaa48684aa4733556a900f08f
naveenkumar sivarajan <sivarajan.naveenkumar@mahindra.com>
2023-02-23 16:35:24 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+resource "azurerm_mssql_server" "mssql" {
+  name                         = var.mssql_name
+  resource_group_name          = var.resource_group_name
+  location                     = var.location
+  version                      = "12.0"
+  administrator_login          = var.administrator_login
+  administrator_login_password = var.administrator_login_password
+}
+
+resource "azurerm_mssql_database" "database" {
+  name           = var.database_name
+  server_id      = azurerm_mssql_server.mssql.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 4
+  read_scale     = true
+  sku_name       = "S0"
+  zone_redundant = true
+
+  threat_detection_policy {
+    state                      = "Disabled"
+  }
+}

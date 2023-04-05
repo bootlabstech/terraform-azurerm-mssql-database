15d3b3b4df26cf0e13545117913d653e3ca7c50c
Silpashree Acharya <silpashreeacharya@Silpashrees-MacBook-Air.local>
2023-04-05 10:10:53 +0530

    varaiblised max size

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ resource "azurerm_mssql_database" "database" {
   server_id      = azurerm_mssql_server.mssql.id
   collation      = "SQL_Latin1_General_CP1_CI_AS"
   license_type   = "LicenseIncluded"
-  max_size_gb    = 4
-  read_scale     = true
-  sku_name       = "S0"
-  zone_redundant = true
+  max_size_gb    = var.max_size_gb
+  read_scale     = var.resource
+  sku_name       = var.sku_name
+  zone_redundant = var.zone_redundant
 
   threat_detection_policy {
     state                      = "Enabled"
--- variables.tf
+++ variables.tf
@@ -27,3 +27,22 @@ variable "administrator_login_password" {
   type        = string
   description = "name of the administrator_login_password"
 }
+
+variable "read_scale" {
+  type        = bool
+  description = "read_scale"
+}
+variable "max_size_gb" {
+  type        = number
+  description = "max_size_gb"
+}
+variable "sku_name" {
+  type        = string
+  description = "sku_name"
+}
+variable " zone_redundant" {
+  type        = bool
+  description = " zone_redundant"
+}
+
+
